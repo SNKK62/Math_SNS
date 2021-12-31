356d9d91983eb6a4a4eb961346054e3c5fd7b6b7
SNKK62 <koki.seno62@gmail.com>
2021-12-31 20:43:27 +0900

    12/31-11

--- src/components/axios.ts
+++ src/components/axios.ts
@@ -5,3 +5,20 @@ axios.defaults.xsrfHeaderName = 'X-CSRF-Token'
 axios.defaults.withCredentials = true
 
 export default axios
+// const client = axios.create();
+
+// client.interceptors.request.use(
+//   (config) => {
+//     const token = document.querySelector<HTMLMetaElement>(
+//       'meta[name="csrf-token"]'
+//     )?.content;
+
+//     config.headers["X-CSRF-Token"] = token;
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default client;
